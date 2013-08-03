fa734e46050d3f0a5f32a8ca85860a1aa17138ff
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2013-08-03 12:12:34 +0500

    Few more functions added
    Few more functions added

--- SQLiteHelperDemo/ViewController.h
+++ SQLiteHelperDemo/ViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    IBOutlet UITextView *txtView;
+    
+    NSString *queryResults;
+}
 @end
--- SQLiteHelperDemo/ViewController.m
+++ SQLiteHelperDemo/ViewController.m
@@ -21,30 +21,58 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     [ZeeSQLiteHelper initializeSQLiteDB];
-    
-//    NSString *query = @"select * from recipes";
-//    NSString *query = [NSString stringWithFormat:@"SELECT DISTINCT `key` FROM (SELECT SUBSTR(`title`, 1, 1) AS `key` FROM `recipes`WHERE `category_ids` LIKE '%%2,%%' OR `category_ids` LIKE '%%,2' ORDER BY `title` ASC ) AS `temp` GROUP BY `key`"];
-    
-//    NSString *query = @"SELECT * FROM recipes ORDER BY ROWID ASC LIMIT 1";
-    
-    
-//    NSLog(@"%@",[ZeeSQLiteHelper readQueryFromDB:query]);
-    
-//    NSLog(@"%d",[ZeeSQLiteHelper getTotalNumberOfRowsOfTable:@"recipes"]);
-    
-    
-//    NSLog(@"%@ column count: %d",[ZeeSQLiteHelper getAllColumnTitlesOfTable:@"recipes"],[[ZeeSQLiteHelper getAllColumnTitlesOfTable:@"recipes"] count]);
-    
-    
-//    NSString *query = @"PRAGMA table_info(recipes)";
-    
-//    NSLog(@"schema %@",[ZeeSQLiteHelper readQueryFromDB:query]);
+    queryResults = [[NSString alloc] init];
+    [self getTableInformation];
+    [self getAllColumnTitles];
+    [self getRowCountOfTable];
+    [self getAllRecordsFromTable];
 }
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)getTableInformation
+{
+    NSString *query = @"PRAGMA table_info(recipes)";
+    [ZeeSQLiteHelper initializeSQLiteDB];
+    NSMutableArray *results = [ZeeSQLiteHelper readQueryFromDB:query];
+    [ZeeSQLiteHelper closeDatabase];
+    
+    queryResults = [queryResults stringByAppendingFormat:@"***TABLE INFROMATION***\n%@\n*** ***\n",results];
 
+//    NSLog(@"TableInformation:%@",results);
+}
+-(void)getAllColumnTitles
+{
+    [ZeeSQLiteHelper initializeSQLiteDB];
+    NSMutableArray *results = [ZeeSQLiteHelper getAllColumnTitlesOfTable:@"recipes"];
+    [ZeeSQLiteHelper closeDatabase];
+    
+    queryResults = [queryResults stringByAppendingFormat:@"***COLUMN TITLES***\n%@\n*** ***\n",results];
+    
+//    NSLog(@"Column titles:%@",results);
+}
+-(void)getRowCountOfTable
+{
+    [ZeeSQLiteHelper initializeSQLiteDB];
+    NSInteger results = [ZeeSQLiteHelper getTotalNumberOfRowsOfTable:@"recipes"];
+    [ZeeSQLiteHelper closeDatabase];
+    
+    queryResults = [queryResults stringByAppendingFormat:@"***ROW COUNT***\n%d\n*** ***\n",results];
+    NSLog(@"Row count:%d",results);
+}
+-(void)getAllRecordsFromTable
+{
+    NSString *query = @"SELECT * FROM recipes";
+    [ZeeSQLiteHelper initializeSQLiteDB];
+    NSMutableArray *results = [ZeeSQLiteHelper readQueryFromDB:query];
+    [ZeeSQLiteHelper closeDatabase];
+    
+    queryResults = [queryResults stringByAppendingFormat:@"***ALL RECORDS***\n%@\n*** ***\n",results];
+    
+    [txtView setText:queryResults];
+    
+//    NSLog(@"allRecords: %@",results);
+}
 @end
